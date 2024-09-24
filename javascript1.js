4841e4eb25e65f17e3328498429b085b1c515fb2
riteshcs22 <chauhanritesh9193@gmail.com>
2024-09-24 10:13:12 +0530

    commited

---
+++ javascript1.js
@@ -0,0 +1,71 @@
+//String.js
+
+const name = "Ritesh chauhan
+const repocount = 50
+//console.log(hello my name is ${name} and my repocount is ${repocount})
+const GameName = new String("Riteshch")
+console.log(GameName)
+console.log(GameName.length)
+console.log(GameName.charAt('3'));
+console.log(GameName.slice(2,7));
+console.log(GameName.concat(name));
+console.log(GameName.split())
+console.log(GameName.trim(2 ,4))
+console.log(GameName.toLowerCase());
+//console.log(GameName.replace)
+console.log(GameName.valueOf())
+console.log(GameName+name);
+console.log(GameName.substr(2,1));
+console.log(GameName+name);
+
+
+
+//math.js
+console.log(Math.pow(2,6));
+console.log(Math.floor(1.99999));
+console.log(Math.ceil(1.00001));
+console.log(Math.round(1.55));
+console.log(Math.random()*100);
+console.log(Math.random()*(-14));
+console.log(Math.round(Math.random()*(15-5)+5));
+console.log(Math.pow(3,4));
+
+
+
+//functions.js
+console.log("H")
+console.log("H")
+console.log("H")
+console.log("H")
+console.log("H")
+console.log("H");
+function myname(){
+    console.log("H")
+console.log("H")
+console.log("H")
+console.log("H")
+console.log("H")
+console.log("H");
+
+}
+//myname()
+function add2(number1,number2){
+    console.log(number1+number2)
+
+}
+add2(3,"a")
+function loginUser(username){
+    return ${username}just logged in
+}
+console.log(loginUser("kasim khan"))
+function calculatecarprice(val1, val2,...num1){
+return num1
+}
+//console.log(calculatecarprice(200,400,500,2000))
+const user={
+    username:"kasim khan"
+    price:199
+}
+function handleobject(anyobject){
+    console.log(username $)
+}
